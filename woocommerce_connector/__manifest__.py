ab5f48b08b3d2f48ca361c1c96551f9495089e63
Saad Mujeeb <saadmujeeb123@gmail.com>
2020-04-23 17:51:30 +0500

    new banner and icon

--- woocommerce_connector/__manifest__.py
+++ woocommerce_connector/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
     # any module necessary for this one to work correctly
     'depends': [],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     # always loaded
     'data': [
         'security/ir.model.access.csv',
